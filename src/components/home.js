1ecbf79e30353af22a67eb2d648193dc1caa1260
Kacper <kacperadach@gmail.com>
2017-04-18 18:22:54 -0400

    working on animation

--- src/components/home.js
+++ src/components/home.js
@@ -7,25 +7,24 @@ const Home = React.createClass({
 	render() {
 		return (
 			<div className="main-home">
-				<div className="main-home-image">
-					<div className="main-home-container">
-						<div className="main-top-container">
-							<div className="home-logo-div">
-								<h1><strong>H</strong>ome <strong>L</strong>isting <strong>P</strong>hotography</h1>
-							</div>
-							<div className="home-menu-div">
-								<button>Home</button>
-								<button>Gallery</button>
-								<button>Contact</button>
-							</div>
+				<div className="main-home-image"></div>
+				<div className="main-home-container">
+					<div className="main-top-container">
+						<div className="home-logo-div">
+							<h1><strong>H</strong>ome <strong>L</strong>isting <strong>P</strong>hotography</h1>
 						</div>
-						<div className="main-body-container">
-							<div className="main-body">
-								<h2>We deliver the Highest Quality Professional Real Estate Photography</h2>
-							</div>
-							<div className="main-body-button-div">
-								<button>Checkout Our Portfolio <Glyphicon glyph="home"/></button>
-							</div>
+						<div className="home-menu-div">
+							<button>Home</button>
+							<button>Gallery</button>
+							<button>Contact</button>
+						</div>
+					</div>
+					<div className="main-body-container">
+						<div className="main-body">
+							<h2>We deliver the Highest Quality Professional Real Estate Photography</h2>
+						</div>
+						<div className="main-body-button-div">
+							<button>Checkout Our Portfolio <Glyphicon glyph="home"/></button>
 						</div>
 					</div>
 				</div>
